c97760781d56711b66dd0e9e972dc1c50a5dc3b1
Pradhyuman <pradhyumanp.dharmatechnolabs@gmail.com>
2022-11-03 16:47:42 +0530

    customizing buttons

--- code/Conceptive/Commerce/Block/Index.php
+++ code/Conceptive/Commerce/Block/Index.php
@@ -4,6 +4,9 @@ namespace Conceptive\Commerce\Block;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Catalog\Model\Product;
+use Magento\Sales\Model\ResourceModel\Report\Bestsellers\Collection;
+use Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 
 class Index extends Template
 {
@@ -11,6 +14,12 @@ class Index extends Template
     protected $registry;
 
     protected $product;
+
+    protected $_bestSellers;
+
+    protected $configurable;
+
+    protected $configurableProd;
 
     /**
      * Hello constructor.
@@ -22,11 +31,17 @@ class Index extends Template
         Template\Context $context,
         \Magento\Framework\Registry $registry,
         Product $product,
+        Collection $bestSellers,
+        Configurable $configurable,
+        ProductRepositoryInterface $configurableProd,
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->registry = $registry;
         $this->product = $product;
+        $this->_bestSellers = $bestSellers;
+        $this->configurable = $configurable;
+        $this->configurableProd = $configurableProd;
     }
 
     public function getCurrentProductId(){
@@ -39,5 +54,23 @@ class Index extends Template
         return $this->product->load($product_id);
     }
 
+    public function getProduct($product_id){
+        return $this->product->load($product_id);
+    }
 
+    public function getBestSellerProductsCollection()
+    {
+        $bestProducts = $this->_bestSellers->setPeriod('year')->setPageSize(4);
+        return $bestProducts;
+    }
+
+    public function getConfigurableProdId($productId){
+        $productData = $this->configurable->getParentIdsByChild($productId);
+        return $productData;
+    }
+
+    public function getConfigurableProd($productId){
+        $product = $this->configurableProd->getById($productId);
+        return $product;
+    }
 }
